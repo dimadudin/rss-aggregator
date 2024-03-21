096ecf8aeb286a97d4e9d514d108732a953104b7
dimadudin <dmitriy.i.dudin@gmail.com>
2024-03-21 21:48:58 +0300

    feat: generate users via sqlc

--- handlers.go
+++ handlers.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func handleReady(w http.ResponseWriter, r *http.Request) {
 	response := struct {
@@ -10,5 +16,30 @@ func handleReady(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleError(w http.ResponseWriter, r *http.Request) {
+	RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+}
+
+func (cfg *config) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	request := struct {
+		Name string `json:"name"`
+	}{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&request)
+	if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	response := struct {
+		Id        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"update_at"`
+		Name      string    `json:"name"`
+	}{
+		Id:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      request.Name,
+	}
+	cfg.DB.CreateUser()
 	RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
 }
--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/dimadudin/rss-aggregator/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type config struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("CONN")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err.Error())
+	}
+	dbQueries := database.New(db)
+	cfg := config{DB: dbQueries}
+
 	mux := http.NewServeMux()
+	mux.HandleFunc("POST /v1/users", handleCreateUser)
 	mux.HandleFunc("GET /v1/readiness", handleReady)
 	mux.HandleFunc("GET /v1/error", handleError)
 	corsMux := MwAddCors(mux)
